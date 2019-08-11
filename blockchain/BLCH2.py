05c08b240ad0a65a1484fc2cf0aa992760e68501
lionn <levlevlavrov@gmail.com>
2019-08-11 17:24:35 +0500

    Update BLCH2.py

--- blockchain/BLCH2.py
+++ blockchain/BLCH2.py
@@ -28,98 +28,129 @@ background_label.place(x=0, y=0, relheight=1, relwidth=1)
 frame = tk.Frame(root, bg='indianred', bd=2)
 frame.place(relx=0.1, rely=0.13, relheight=0.1, relwidth=0.8)
 
-def button_cliked():                                   
+entry = tk.Entry(frame, bg='white', font='Times 20')
+entry.place(relx=0, rely=0, relheight=0.9, relwidth=0.7)
+
+a = entry.get()
+
+def first1():
     a = entry.get()
-
-    if int(a) >= 1 :
-        label['text'] = 'Now battery have ' + a + ' units of energy'
-    else:
-        label['text'] = 'Now battery have 0 units of energy'
-
     b = int(a)
     l_frame = tk.Frame(root, bg='lavender', bd=1)
     l_frame.place(relx=0.55, rely=0.43, relheight=0.25, relwidth=0.15)
+    labels = [5, 4, 3, 2, 1]
+
+    label['text'] = 'Now battery have ' + str(b - 15) + ' units of energy'
+
+
+    for i in range(len(labels)):
+        cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
+        cur_label.pack()
+
+
+def second2():
+
+    l_frame = tk.Frame(root, bg='lavender', bd=1)
+    l_frame.place(relx=0.55, rely=0.43, relheight=0.25, relwidth=0.15)
+    labels = [5, 4, 3, 2, 0]
+    label['text'] = 'Now battery have 0 units of energy'
+
+    for i in range(len(labels)):
+        cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
+        cur_label.pack()
+
+def third3():
+    a = entry.get()
+    b = int(a)
+    l_frame = tk.Frame(root, bg='lavender', bd=1)
+    l_frame.place(relx=0.55, rely=0.43, relheight=0.25, relwidth=0.15)
+    labels = [5, 4, 3, b - 12, 0]
+    label['text'] = 'Now battery have 0 units of energy'
+
+    for i in range(len(labels)):
+        cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
+        cur_label.pack()
+
+def forth4():
+    a = entry.get()
+    b = int(a)
+    l_frame = tk.Frame(root, bg='lavender', bd=1)
+    l_frame.place(relx=0.55, rely=0.43, relheight=0.25, relwidth=0.15)
+    labels = [5, 4, b - 9, 0, 0]
+    label['text'] = 'Now battery have 0 units of energy'
+
+    for i in range(len(labels)):
+        cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
+        cur_label.pack()
+
+def fifth5():
+    a = entry.get()
+    b = int(a)
+    l_frame = tk.Frame(root, bg='lavender', bd=1)
+    l_frame.place(relx=0.55, rely=0.43, relheight=0.25, relwidth=0.15)
+    labels = [5, b - 5, 0, 0, 0]
+    label['text'] = 'Now battery have 0 units of energy'
+
+    for i in range(len(labels)):
+        cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
+        cur_label.pack()
+
+def last():
+    l_frame = tk.Frame(root, bg='lavender', bd=1)
+    l_frame.place(relx=0.55, rely=0.43, relheight=0.25, relwidth=0.15)
+    labels = [4, 0, 0, 0, 0]
+    label['text'] = 'Now battery have 0 units of energy'
+
+    for i in range(len(labels)):
+        cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
+        cur_label.pack()
 
 
 
 
-    while b >= 0:
+def button_cliked():
+    a = entry.get()
+    b = int(a)
+    while b >= 1:
 
+        if b >= 15:
+            print('15' + '  now battery have ' + str(b - 15) + ' units of energy')
 
-        n = b - 1
+            # first1()
 
-        if n >= 15:
+        if b >= 14 and b <15:
+            print(str(b) + ' now battery have 0 units of energy')
+            # second2()
 
-            labels = [5, 4, 3, 2, 1]
+        if b >= 12 and b < 14:
+            print(str(b) + ' now battery have 0 units of energy')
+            # third3()
 
-            label['text'] = 'Now battery have ' + str(b-15) + ' units of energy'
+        if b >= 9 and b < 12:
+            print(str(b) + ' now battery have 0 units of energy')
+            # forth4()
 
-            for i in range(len(labels)):
-                cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
-                cur_label.pack()
+        if b >= 5 and b < 9:
 
-        elif n >= 14:
+            print(str(b) + ' now battery have 0 units of energy')
+            # fifth5()
 
-            labels = [5, 4, 3, 2, 0]
-            label['text'] = 'Now battery have 0 units of energy'
+        if b < 5:
 
-            for i in range(len(labels)):
-                cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
-                cur_label.pack()
-        elif n >= 12:
+            print(str(b) + ' now battery have 0 units of energy')
+            # last()
 
-            labels = [5, 4, 3, b-12, 0]
-            label['text'] = 'Now battery have 0 units of energy'
+        if b == 0 :
+            print('0 now battery have 0 units of energy')
+            break
 
-            for i in range(len(labels)):
-                cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
-                cur_label.pack()
-        elif n >= 9:
-
-            labels = [5, 4, b-9, 0, 0]
-            label['text'] = 'Now battery have 0 units of energy'
-
-            for i in range(len(labels)):
-                cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
-                cur_label.pack()
-
-        elif n >= 5:
-
-            labels = [5, b-5, 0, 0, 0]
-            label['text'] = 'Now battery have 0 units of energy'
-
-            for i in range(len(labels)):
-                cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
-                cur_label.pack()
-        elif n >= 0:
-
-            labels = [b, 0, 0, 0, 0]
-            label['text'] = 'Now battery have 0 units of energy'
-
-            for i in range(len(labels)):
-                cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
-                cur_label.pack()
-
-
-    # a =
-    # c =
-    # d =
-    # e =
-    # o =
-    #
-    # # labels = [a, c, d, e, o]
-    # while b >=1:
-    #     print(1)
-    #     time.sleep(1)
-
+        b = b - 1
+        time.sleep(1)
 
 
 
 low_frame = tk.Frame(root, bg='gray', bd=1 )
 low_frame.place(relx=0.12, rely=0.31, relheight=0.06, relwidth=0.74)
-
-entry = tk.Entry(frame, bg='white', font='Times 20')
-entry.place(relx=0, rely=0, relheight=0.9, relwidth=0.7)
 
 label = tk.Label(low_frame, text='Now battery have 0 units of energy', bg='white', anchor='nw', fg='blue', font='Times 16')
 label.pack()
@@ -138,10 +169,15 @@ for i in range(len(labels)):
     cur_label = tk.Label(lo_frame, text=labels[i],bg='lavender', anchor='w', font='Times 12')
     cur_label.pack()
 
-
-
-
+def numbers(labels):
+    l_frame = tk.Frame(root, bg='lavender', bd=1)
+    l_frame.place(relx=0.55, rely=0.43, relheight=0.25, relwidth=0.15)
+    labels = [0, 0, 0, 0, 0]
+    for i in range(len(labels)):
+        cur_label = tk.Label(l_frame, text=labels[i], bg='lavender', anchor='w', font='Times 12')
+        cur_label.pack()
 
 
 if __name__ == '__main__':
     root.mainloop()
+
